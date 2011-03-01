d64b2b6d1ce79a656c21fc44b53fd8549f66d25f
Roman Stoffel <roman.stoffel@gmail.com>
2011-03-01 23:54:47 +0100

    rs: first commit of our linqpad driver

---
+++ db4oPad/Db4oDriver.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Gamlor.Db4oPad.GUI;
+using LINQPad.Extensibility.DataContext;
+
+namespace Gamlor.Db4oPad
+{
+    public class Db4oDriver : DynamicDataContextDriver
+    {
+        public override string GetConnectionDescription(IConnectionInfo cxInfo)
+        {
+            return Path.GetFileName(cxInfo.CustomTypeInfo.CustomMetadataPath);
+        }
+
+        public override bool ShowConnectionDialog(IConnectionInfo cxInfo, bool isNewConnection)
+        {
+            var dialog = new ConnectDialog(cxInfo);
+            var result = dialog.ShowDialog();
+            return result.HasValue && result.Value;
+        }
+
+        public override string Name
+        {
+            get { return "db4o Driver"; }
+        }
+
+        public override string Author
+        {
+            get { return "Roman Stoffel"; }
+        }
+
+        public override List<ExplorerItem> GetSchemaAndBuildAssembly(IConnectionInfo cxInfo, AssemblyName assemblyToBuild, ref string nameSpace, ref string typeName)
+        {
+            return new List<ExplorerItem>();
+        }
+    }
+}
---
+++ db4oPad/GUI/ConnectDialog.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LINQPad.Extensibility.DataContext;
+
+namespace Gamlor.Db4oPad.GUI
+{
+    /// <summary>
+    /// Interaction logic for ConnectDialog.xaml
+    /// </summary>
+    public partial class ConnectDialog : Window
+    {
+        private IConnectionInfo contextInfo;
+
+        public ConnectDialog(IConnectionInfo cxInfo)
+        {
+            DataContext = cxInfo.CustomTypeInfo;
+            this.contextInfo = cxInfo;
+            InitializeComponent();
+        }
+
+        private void BrowseAssembly(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog
+                          {
+                              FileName = "",
+                              DefaultExt = ".db4o",
+                              Filter = "db4o databases (.db4o)|*.db4o;*.yap|All files (*.*)|*.*"
+                          };
+            var result = dialog.ShowDialog();
+
+            if (result == true)
+            {
+                contextInfo.CustomTypeInfo.CustomMetadataPath = dialog.FileName;
+            }
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
